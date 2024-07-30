38fd4944adf6d76df1ebd93ac242da668642602b
Fadhil Amadan <fadhil@ethicaljobs.com.au>
2024-07-30 10:35:20 +0700

    refactor(component): chapter 16 metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from 'next';
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://fam-learn-next-app-router.vercel.app'),
+};
 
 export default function RootLayout({
   children,
